f1f5429202d183bd6675e58eb91667398a703768
Данила Семашко <59087699+danilasemashko@users.noreply.github.com>
2021-03-16 09:37:16 +0300

    Update train.py

--- train.py
+++ train.py
@@ -50,7 +50,6 @@ def create_dataset(filenames, batch_size):
     return tf.data.TFRecordDataset(filenames) \
         .map(parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE) \
         .cache() \
-        .map(normalize) \
         .batch(batch_size) \
         .prefetch(tf.data.AUTOTUNE)
 
