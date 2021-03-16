ac8aa221b26a1944567c52b573493162f3b4ac88
Данила Семашко <59087699+danilasemashko@users.noreply.github.com>
2021-03-16 10:58:53 +0300

    Update train.py

--- train.py
+++ train.py
@@ -33,13 +33,11 @@ def parse_proto_example(proto):
     }
     example = tf.io.parse_single_example(proto, keys_to_features)
     example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
-    example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.float32)
+    example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.int32)
     example['image'] = tf.image.resize(example['image'], tf.constant([RESIZE_TO, RESIZE_TO]))
     return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
 
 
-def normalize(image, label):
-    return tf.image.per_image_standardization(image), label
 
 
 def create_dataset(filenames, batch_size):
