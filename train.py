38991c2ad9df16b8d21fc0b1d7099360da4fa5d2
Данила Семашко <59087699+danilasemashko@users.noreply.github.com>
2021-03-19 13:51:36 +0300

    Update train.py

--- train.py
+++ train.py
@@ -67,8 +67,8 @@ def build_model():
 
 def step_decay(epoch):
     initial_rate = 0.1
-    drop = 0.5
-    epochs_drop = 5
+    drop = 0.1
+    epochs_drop = 10
     lr = initial_rate * pow(drop, (epoch//epochs_drop)) 
     print(f'{lr}')
     return lr
