f16ea6f8abef9256de7e2237186dd30df87b47a9
Данила Семашко <59087699+danilasemashko@users.noreply.github.com>
2021-03-26 16:51:39 +0300

    Update train.py

--- train.py
+++ train.py
@@ -5,24 +5,24 @@ to classify X-Ray chest images as a lab example for BSU students.
 __author__ = 'Alexander Soroka, soroka.a.m@gmail.com'
 __copyright__ = """Copyright 2020 Alexander Soroka"""
 
+
 import argparse
 import glob
-import math
-
 import numpy as np
 import tensorflow as tf
 import time
 from tensorflow.python import keras as keras
 from tensorflow.python.keras.callbacks import LearningRateScheduler
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.layers.experimental import preprocessing
-from tensorflow.keras.models import Sequential
 from math import exp
+import albumentations as A
+from PIL import Image
+import os
 
 # Avoid greedy memory allocation to allow shared GPU usage
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+  tf.config.experimental.set_memory_growth(gpu, True)
+
 
 LOG_DIR = 'logs'
 BATCH_SIZE = 32
@@ -32,93 +32,90 @@ TRAIN_SIZE = 12786
 
 
 def parse_proto_example(proto):
-    keys_to_features = {
-        'image/encoded': tf.io.FixedLenFeature((), tf.string, default_value=''),
-        'image/label': tf.io.FixedLenFeature([], tf.int64, default_value=tf.zeros([], dtype=tf.int64))
-    }
-    example = tf.io.parse_single_example(proto, keys_to_features)
-    example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
-    example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.uint8)
-    example['image'] = tf.image.resize(example['image'], tf.constant([RESIZE_TO, RESIZE_TO]))
-    return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
+  keys_to_features = {
+    'image/encoded': tf.io.FixedLenFeature((), tf.string, default_value=''),
+    'image/label': tf.io.FixedLenFeature([], tf.int64, default_value=tf.zeros([], dtype=tf.int64))
+  }
+  example = tf.io.parse_single_example(proto, keys_to_features)
+  example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
+  example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.uint8)
+  example['image'] = tf.image.resize(example['image'], tf.constant([RESIZE_TO, RESIZE_TO]))
+  return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
 
 
 def normalize(image, label):
-    return tf.image.per_image_standardization(image), label
-
-
-
-
-
+  return tf.image.per_image_standardization(image), label
+  
+def procces_data(image, label):
+  img = tf.image.adjust_contrast(image, 2)
+  img = tf.image.adjust_brightness(img, 0.3)
+  return img, label
+  
 def create_dataset(filenames, batch_size):
-    """Create dataset from tfrecords file
-    :tfrecords_files: Mask to collect tfrecords file of dataset
-    :returns: tf.data.Dataset
-    """
-    return tf.data.TFRecordDataset(filenames) \
-        .map(parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE) \
-        .cache() \
-        .batch(batch_size) \
-        .prefetch(tf.data.AUTOTUNE)
-
-
-img_augmentation = Sequential(
-    [
-        preprocessing.RandomRotation(factor=0.4)
-    ],
-    name="img_augmentation",
-)
+  """Create dataset from tfrecords file
+  :tfrecords_files: Mask to collect tfrecords file of dataset
+  :returns: tf.data.Dataset
+  """
+  return tf.data.TFRecordDataset(filenames)\
+    .map(parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE)\
+    .cache()\
+    .map(procces_data)\
+    .batch(batch_size)\
+    .prefetch(tf.data.AUTOTUNE)
 
 
 def build_model():
-    inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-    x = img_augmentation(inputs)
-    model = tf.keras.applications.EfficientNetB0(include_top=False, input_tensor=x, weights='imagenet')
-    model.trainable = False
-    x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
-    outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
-    return tf.keras.Model(inputs=inputs, outputs=outputs)
+  inputs = tf.keras.Input(shape=(224, 224, 3))
+  model = tf.keras.applications.EfficientNetB0(include_top=False, input_tensor=inputs, weights='imagenet')
+  model.trainable = False
+  x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
+  outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
+  return tf.keras.Model(inputs=inputs, outputs=outputs)
 
 
 def exp_decay(epoch):
     initial_rate = 0.1
-    k = 0.1
+    k = 0.3
     lr = initial_rate * exp(-k*epoch)
     print(f'{lr}')
     return lr
 
 
-
 def main():
-    args = argparse.ArgumentParser()
-    args.add_argument('--train', type=str,
-                      help='Glob pattern to collect train tfrecord files, use single quote to escape *')
-    args = args.parse_args()
+  args = argparse.ArgumentParser()
+  args.add_argument('--train', type=str, help='Glob pattern to collect train tfrecord files, use single quote to escape *')
+  args = args.parse_args()
 
-    dataset = create_dataset(glob.glob(args.train), BATCH_SIZE)
-    train_size = int(TRAIN_SIZE * 0.7 / BATCH_SIZE)
-    train_dataset = dataset.take(train_size)
-    validation_dataset = dataset.skip(train_size)
+  dataset = create_dataset(glob.glob(args.train), BATCH_SIZE)
+  train_size = int(TRAIN_SIZE * 0.7 / BATCH_SIZE)
+  train_dataset = dataset.take(train_size)
+  validation_dataset = dataset.skip(train_size)
+  model = build_model()
+  """
+  for x, y in dataset.take(1):
+    for j in x:
+      print(j)
+      #tf.keras.preprocessing.image.save_img(path=LOG_DIR, x=j, file_format='.jpg')
+      img = Image.fromarray(j.numpy(), 'RGB')
+      img.save('img.jpg')
+      break
+  """
+  model.compile(
+    optimizer=tf.optimizers.Adam(),
+    loss=tf.keras.losses.categorical_crossentropy,
+    metrics=[tf.keras.metrics.categorical_accuracy],
+  )
 
-    l_rate = LearningRateScheduler(exp_decay)
-    model = build_model()
-    model.compile(
-        optimizer=tf.optimizers.Adam(),
-        loss=tf.keras.losses.categorical_crossentropy,
-        metrics=[tf.keras.metrics.categorical_accuracy],
-    )
-
-    log_dir = '{}/owl-{}'.format(LOG_DIR, time.time())
-    model.fit(
-        train_dataset,
-        epochs=50,
-        validation_data=validation_dataset,
-        callbacks=[
-            tf.keras.callbacks.TensorBoard(log_dir),
-            l_rate
-        ]
-    )
-
+  log_dir='{}/owl-{}'.format(LOG_DIR, time.time())
+  model.fit(
+    train_dataset,
+    epochs=50,
+    validation_data=validation_dataset,
+    callbacks=[
+      tf.keras.callbacks.TensorBoard(log_dir),
+      LearningRateScheduler(exp_decay)
+    ]
+  )
 
 if __name__ == '__main__':
     main()
