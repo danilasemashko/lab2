0dd218ddc9f4ce19a3a7130969b8277c9bb9267e
Данила Семашко <59087699+danilasemashko@users.noreply.github.com>
2021-03-27 13:39:55 +0300

    Update train.py

--- train.py
+++ train.py
@@ -86,13 +86,7 @@ def main():
   validation_dataset = dataset.skip(train_size)
   model = build_model()
 
-  for x, y in dataset.take(1):
-    for j in x:
-      print(j)
-      #tf.keras.preprocessing.image.save_img(path=LOG_DIR, x=j, file_format='.jpg')
-      img = Image.fromarray(j.numpy(), 'RGB')
-      img.save('img.jpg')
-      break
+ 
 
   model.compile(
     optimizer=tf.optimizers.Adam(),
