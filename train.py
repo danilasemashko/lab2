cbf2829b9cd283af4188e7c0b40c0ca24a138b0b
Данила Семашко <59087699+danilasemashko@users.noreply.github.com>
2021-03-28 20:14:22 +0300

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ to classify X-Ray chest images as a lab example for BSU students.
 """
 __author__ = 'Alexander Soroka, soroka.a.m@gmail.com'
 __copyright__ = """Copyright 2020 Alexander Soroka"""
-
 import argparse
 import glob
 import numpy as np
@@ -12,19 +11,17 @@ import time
 from tensorflow.python import keras as keras
 from tensorflow.python.keras.callbacks import LearningRateScheduler
 from math import exp
-import os
 
+import os
 # Avoid greedy memory allocation to allow shared GPU usage
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
   tf.config.experimental.set_memory_growth(gpu, True)
-  
 LOG_DIR = 'logs'
 BATCH_SIZE = 32
 NUM_CLASSES = 20
 RESIZE_TO = 224
 TRAIN_SIZE = 12786
-
 def parse_proto_example(proto):
   keys_to_features = {
     'image/encoded': tf.io.FixedLenFeature((), tf.string, default_value=''),
@@ -33,17 +30,15 @@ def parse_proto_example(proto):
   example = tf.io.parse_single_example(proto, keys_to_features)
   example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
   example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.uint8)
+
   example['image'] = tf.image.resize(example['image'], tf.constant([250, 250]))
-  example['image'] = tf.image.resize(example['image'], tf.constant([300, 300]))
   return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
 
 
 def normalize(image, label):
   return tf.image.per_image_standardization(image), label
-
 def process_data(image, label):
   return tf.image.random_crop(image, [224, 224, 3]), label
-
 def create_dataset(filenames, batch_size):
   """Create dataset from tfrecords file
   :tfrecords_files: Mask to collect tfrecords file of dataset
@@ -52,18 +47,16 @@ def create_dataset(filenames, batch_size):
   return tf.data.TFRecordDataset(filenames)\
     .map(parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE)\
     .cache()\
-    .map(process_data)
+    .map(process_data)\
     .batch(batch_size)\
     .prefetch(tf.data.AUTOTUNE)
-  
 def build_model():
-  inputs = tf.keras.Input(shape=(250, 250, 3))
+  inputs = tf.keras.Input(shape=(224, 224, 3))
   model = tf.keras.applications.EfficientNetB0(include_top=False, input_tensor=inputs, weights='imagenet')
   model.trainable = False
   x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
   outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
-
 def exp_decay(epoch):
     initial_rate = 0.1
     k = 0.3
@@ -74,7 +67,6 @@ def main():
   args = argparse.ArgumentParser()
   args.add_argument('--train', type=str, help='Glob pattern to collect train tfrecord files, use single quote to escape *')
   args = args.parse_args()
-  
   dataset = create_dataset(glob.glob(args.train), BATCH_SIZE)
   train_size = int(TRAIN_SIZE * 0.7 / BATCH_SIZE)
   train_dataset = dataset.take(train_size)
