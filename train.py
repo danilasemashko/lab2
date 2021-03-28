5704518df5587ce36d4d52aa36b2b6d822deaf93
Данила Семашко <59087699+danilasemashko@users.noreply.github.com>
2021-03-28 19:57:56 +0300

    Update train.py

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ __author__ = 'Alexander Soroka, soroka.a.m@gmail.com'
 __copyright__ = """Copyright 2020 Alexander Soroka"""
 
 
+
 import argparse
 import glob
 import numpy as np
@@ -13,20 +14,25 @@ import tensorflow as tf
 import time
 from tensorflow.python import keras as keras
 from tensorflow.python.keras.callbacks import LearningRateScheduler
-from tensorflow.keras.applications import EfficientNetB0
-import math
+from math import exp
+import os
+
+
 
 # Avoid greedy memory allocation to allow shared GPU usage
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
   tf.config.experimental.set_memory_growth(gpu, True)
-
-
+  
+  
+  
 LOG_DIR = 'logs'
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 NUM_CLASSES = 20
 RESIZE_TO = 224
 TRAIN_SIZE = 12786
+
+
 
 
 def parse_proto_example(proto):
@@ -37,14 +43,15 @@ def parse_proto_example(proto):
   example = tf.io.parse_single_example(proto, keys_to_features)
   example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
   example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.uint8)
-  example['image'] = tf.image.resize(example['image'], tf.constant([RESIZE_TO, RESIZE_TO]))
+  example['image'] = tf.image.resize(example['image'], tf.constant([350, 350]))
   return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
 
-def contrast(image,label):
-  return tf.image.adjust_contrast(image, 0.5), label
 
-def brightness(image, label):
-  return tf.image.adjust_brightness(image, delta=0.1),label
+def normalize(image, label):
+  return tf.image.per_image_standardization(image), label
+
+def process_data(image, label):
+  return tf.image.random_crop(image, [224, 224, 3]), label
 
 def create_dataset(filenames, batch_size):
   """Create dataset from tfrecords file
@@ -54,45 +61,47 @@ def create_dataset(filenames, batch_size):
   return tf.data.TFRecordDataset(filenames)\
     .map(parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE)\
     .cache()\
+    .map(process_data)\
     .batch(batch_size)\
     .prefetch(tf.data.AUTOTUNE)
 
-def exp_decay(epoch,lr):
-  initial_lrate = 0.1
-  k = 0.3
-  lrate = initial_lrate * math.exp(-k*epoch)
-  return lrate
 
 
 def build_model():
-  inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  x = tf.keras.layers.experimental.preprocessing.RandomRotation(factor=(0, 0.02), fill_mode='wrap')(inputs)
-  model = EfficientNetB0(input_tensor=x,include_top=False,pooling='avg', weights='imagenet')
-  model.trainable=False
-  x = tf.keras.layers.Flatten()(model.output)
-  outputs = tf.keras.layers.Dense(NUM_CLASSES, activation = tf.keras.activations.softmax)(x)
+  inputs = tf.keras.Input(shape=(224, 224, 3))
+  model = tf.keras.applications.EfficientNetB0(include_top=False, input_tensor=inputs, weights='imagenet')
+  model.trainable = False
+  x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
+  outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 
 
+
+def exp_decay(epoch):
+    initial_rate = 0.1
+    k = 0.3
+    lr = initial_rate * exp(-k*epoch)
+    print(f'{lr}')
+    return lr
+  
+  
 def main():
   args = argparse.ArgumentParser()
   args.add_argument('--train', type=str, help='Glob pattern to collect train tfrecord files, use single quote to escape *')
   args = args.parse_args()
-
+  
   dataset = create_dataset(glob.glob(args.train), BATCH_SIZE)
   train_size = int(TRAIN_SIZE * 0.7 / BATCH_SIZE)
   train_dataset = dataset.take(train_size)
   validation_dataset = dataset.skip(train_size)
-
   model = build_model()
-  print(model.summary())
-
+  
   model.compile(
     optimizer=tf.optimizers.Adam(),
     loss=tf.keras.losses.categorical_crossentropy,
     metrics=[tf.keras.metrics.categorical_accuracy],
   )
-
+  
   log_dir='{}/owl-{}'.format(LOG_DIR, time.time())
   model.fit(
     train_dataset,
@@ -103,7 +112,6 @@ def main():
       LearningRateScheduler(exp_decay)
     ]
   )
-
-
+  
 if __name__ == '__main__':
     main()
