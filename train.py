5878ef57794295417b5b883f90d942b2112334a4
Данила Семашко <59087699+danilasemashko@users.noreply.github.com>
2021-04-06 02:04:10 +0300

    Update train.py

--- train.py
+++ train.py
@@ -108,7 +108,7 @@ def main():
   log_dir='{}/owl-{}'.format(LOG_DIR, time.time())
   model.fit(
     train_dataset,
-    epochs=15,
+    epochs=20,
     validation_data=validation_dataset,
     callbacks=[
       tf.keras.callbacks.TensorBoard(log_dir),
